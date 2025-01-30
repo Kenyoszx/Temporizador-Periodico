3a042bf268dc338ca2e555feaa1907f64960a99f
João Victor de Souza Silva <joao1452019@gmail.com>
2025-01-30 12:48:32 -0300

    Inicialização dos pinos e rotina de repetição

--- semaforo.c
+++ semaforo.c
@@ -3,26 +3,44 @@
 #include "hardware/timer.h"
 
 void init_pins();
-bool repeating_timer_callback(struct repeating_timer);
+bool repeating_timer_callback(struct repeating_timer *);
 
-
-struct repeating_timer timer;
-
-
+#define LED_PIN_GREEN 11
+#define LED_PIN_BLUE 12
+#define LED_PIN_RED 13
 
 int main()
 {
-    stdio_init_all();
-    init_pins();
+    stdio_init_all(); //Inicializa entrada e saída padrão
+    init_pins(); //Inicializa os Pinos
+    struct repeating_timer timer; // Declaração de uma struct de temporizador de repetição.
 
-    add_repeating_timer_ms(3000,repeating_timer_callback,NULL,&timer);
+    // Configura o temporizador para chamar a função de callback a cada 3 segundos.
+    add_repeating_timer_ms(3000, repeating_timer_callback, NULL, &timer);
 
-    while (true) {
+    while (true)
+    {
         printf("Hello, world!\n");
         sleep_ms(1000);
     }
 }
-bool repeating_timer_callback(struct repeating_timer *t) {
-    printf("Abrorbora");
+void init_pins()
+{
+    //Inicializa os pinos dos leds
+    gpio_init(LED_PIN_GREEN);
+    gpio_set_dir(LED_PIN_GREEN, GPIO_OUT);
+    gpio_put(LED_PIN_GREEN, 0);
+
+    gpio_init(LED_PIN_BLUE);
+    gpio_set_dir(LED_PIN_BLUE, GPIO_OUT);
+    gpio_put(LED_PIN_BLUE, 0);
+
+    gpio_init(LED_PIN_RED);
+    gpio_set_dir(LED_PIN_RED, GPIO_OUT);
+    gpio_put(LED_PIN_RED, 0);
+}
+bool repeating_timer_callback(struct repeating_timer *t)
+{
+    
     return true;
 }
